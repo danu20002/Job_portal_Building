088cafa7988f405a5aaea9bbf04315fbbf9a4292
Danesh Naik <naikdanesh2@gmail.com>
2023-10-04 08:43:35 +0530

    changed

--- app/src/main/java/com/daneshnaik/jobsearchapp/learning.java
+++ app/src/main/java/com/daneshnaik/jobsearchapp/learning.java
@@ -2,22 +2,51 @@ package com.daneshnaik.jobsearchapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class learning extends AppCompatActivity {
     BottomNavigationView bottom_navigation_learning;
+    AppCompatButton logout_btn;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning);
         bottom_navigation_learning=findViewById(R.id.bottom_navigation_home);
         bottom_navigation_learning.setSelectedItemId(R.id.applications);
+        logout_btn=findViewById(R.id.logout_btn);
+
+
+        auth=FirebaseAuth.getInstance();
+
+
+        logout_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.signOut();
+                startActivity(new Intent(getApplicationContext(), Enter_Activity.class));
+                finish();
+            }
+        });
+
+
+
+
+
+
+
+
+
+
 
         bottom_navigation_learning.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
 
