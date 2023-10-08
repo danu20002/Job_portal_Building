3d348afe69dc3ac0395d52728365079638b328c9
Danesh Naik <naikdanesh2@gmail.com>
2023-10-08 17:58:00 +0530

    changed bit

--- app/src/main/java/com/daneshnaik/jobsearchapp/First_Activity.java
+++ app/src/main/java/com/daneshnaik/jobsearchapp/First_Activity.java
@@ -3,10 +3,10 @@ package com.daneshnaik.jobsearchapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+
 import android.os.Bundle;
 import android.os.Handler;
 
-import com.google.android.gms.common.data.DataHolder;
 
 public class First_Activity extends AppCompatActivity {
 
@@ -17,10 +17,13 @@ public class First_Activity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+                startActivity(new Intent(getApplicationContext(), Enter_Activity.class));
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
                 finish();
             }
-        },2000);
+        }, 3000);
+
+
     }
+
 }
